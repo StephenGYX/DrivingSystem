a7a7f3e137f80863b2de260b1969c0aefc1a7b6f
StephenGYX <471733780@qq.com>
2020-02-16 00:23:39 +0800

    2020/02/15

--- src/main/java/com/drivingsys/dao/BackMenuMapper.java
+++ src/main/java/com/drivingsys/dao/BackMenuMapper.java
@@ -3,9 +3,11 @@ package com.drivingsys.dao;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+
 @Mapper
 @Repository
 public interface BackMenuMapper
 {
 	
+
 }
