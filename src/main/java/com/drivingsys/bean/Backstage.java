3f298f4e977763903a24ed2ea0785d703b4ea1f7
StephenGYX <471733780@qq.com>
2020-02-15 21:27:10 +0800

    后台用户登录

---
+++ src/main/java/com/drivingsys/bean/Backstage.java
@@ -0,0 +1,66 @@
+package com.drivingsys.bean;
+
+public class Backstage {
+
+  private long bid;
+  private String baccount;
+  private String bpassword;
+  private String bname;
+  private long rid;
+  private String bstate;
+
+
+  public long getBid() {
+    return bid;
+  }
+
+  public void setBid(long bid) {
+    this.bid = bid;
+  }
+
+
+  public String getBaccount() {
+    return baccount;
+  }
+
+  public void setBaccount(String baccount) {
+    this.baccount = baccount;
+  }
+
+
+  public String getBpassword() {
+    return bpassword;
+  }
+
+  public void setBpassword(String bpassword) {
+    this.bpassword = bpassword;
+  }
+
+
+  public String getBname() {
+    return bname;
+  }
+
+  public void setBname(String bname) {
+    this.bname = bname;
+  }
+
+
+  public long getRid() {
+    return rid;
+  }
+
+  public void setRid(long rid) {
+    this.rid = rid;
+  }
+
+
+  public String getBstate() {
+    return bstate;
+  }
+
+  public void setBstate(String bstate) {
+    this.bstate = bstate;
+  }
+
+}
