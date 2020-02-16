1fb1d8bd6ee989fdbe360cc72a851f6ae2d36180
gkpk1560 <841126293@qq.com>
2020-02-16 14:45:56 +0800

    角色菜单

--- src/main/java/com/drivingsys/dao/BackMenuMapper.java
+++ src/main/java/com/drivingsys/dao/BackMenuMapper.java
@@ -2,10 +2,10 @@ package com.drivingsys.dao;
 
 import com.drivingsys.bean.RoleMenu;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 
 @Mapper
 @Repository
@@ -13,5 +13,4 @@ public interface BackMenuMapper
 {
 
 	public List<RoleMenu> queryRoleMenu(int rid);
-
 }
