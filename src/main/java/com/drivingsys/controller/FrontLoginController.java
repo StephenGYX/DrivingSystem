f32de1c13f95f17aae9418dacb971a848b42cff0
gkpk1560 <841126293@qq.com>
2020-02-16 20:49:28 +0800

    0216 gan

--- src/main/java/com/drivingsys/controller/FrontLoginController.java
+++ src/main/java/com/drivingsys/controller/FrontLoginController.java
@@ -1,5 +1,7 @@
 package com.drivingsys.controller;
 
+import com.drivingsys.bean.Consumer;
+import com.drivingsys.bean.Drivingschool;
 import com.drivingsys.bean.Practise;
 import com.drivingsys.service.BackStageService;
 import com.drivingsys.service.FrontLoginService;
@@ -28,38 +30,65 @@ public class FrontLoginController
 
 
 	@RequestMapping("hello")
-	public String testMain(@RequestParam Map<String,String> reqMap, HttpServletRequest request){
+	public String testMain(@RequestParam Map<String, String> reqMap, HttpServletRequest request)
+	{
 		System.out.println(reqMap);
-//		System.out.println("hellos");
-		String roleid=request.getSession().getAttribute("roleid")+"";
+		//		System.out.println("hellos");
+		String roleid = request.getSession().getAttribute("roleid") + "";
 
-		if (roleid.equals("null")){
-			roleid="4";
-		}
-		System.out.println("roleid="+roleid);
-		 Practise practise = frontLoginService.queryPractiseAccount(reqMap);
-		if (practise == null)
+		if (roleid.equals("null"))
 		{
-			System.out.println("没找到");
-		}else{
-			System.out.println("找到了");
-		};
-		 		return "index.jsp";
+			roleid = "4";
+		}
+		System.out.println("roleid=" + roleid);
+		if (roleid.equals("3"))
+		{
+			Practise practise = frontLoginService.queryPractiseAccount(reqMap);
+			if (practise == null)
+			{
+				System.out.println("没找到教练");
+				return "frontlogin";
+			} else
+			{
+				System.out.println("找到了教练");
+				return "index";
+			}
 
+
+
+		} else if (roleid.equals("2"))
+		{
+			Drivingschool drivingschool=frontLoginService.queryDrivingschool(reqMap);
+			if (drivingschool==null){System.out.println("没找到驾校");
+				return "frontlogin";}
+		else {System.out.println("找到了驾校");
+				return "index";}
+		}else if (roleid.equals("4"))
+		{
+			Consumer consumer=frontLoginService.queryConsumer(reqMap);
+			if (consumer==null){System.out.println("没找到学生");
+				return "frontlogin";}
+			else {System.out.println("找到了学生");
+				return "index";}
+		}
+		return "frontlogin";
 	}
+
 	@RequestMapping("roleid")
 	@ResponseBody
-	public String testMain(String roleid, HttpServletRequest request){
-		System.out.println("roleid方法参数"+roleid);
+	public String testMain(String roleid, HttpServletRequest request)
+	{
+		System.out.println("roleid方法参数" + roleid);
 		String roleid1 = "4";
-		roleid1=roleid;
-		request.getSession().setAttribute("roleid",roleid1);
+		roleid1 = roleid;
+		request.getSession().setAttribute("roleid", roleid1);
 		System.out.println(roleid1);
-return roleid1;
+		return roleid1;
 	}
 
 	@RequestMapping("/index")
-	public String index(){
+	public String index()
+	{
 		return "Getting start from spring boot!";
 	}
 }
