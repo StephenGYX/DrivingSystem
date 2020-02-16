5afa1b64e5ff04b7b20f731506dbf80b2d3df6e6
gkpk1560 <841126293@qq.com>
2020-02-16 14:47:23 +0800

    角色菜单

--- src/main/java/com/drivingsys/controller/BackStageController.java
+++ src/main/java/com/drivingsys/controller/BackStageController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.swing.text.View;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,21 +33,20 @@ public class BackStageController
 
 	@ResponseBody
 	@RequestMapping("/backLogin")
-	public HashMap<String, ArrayList<RoleMenu>> backLogin(@RequestParam Map<String,String> reqMap, HttpServletRequest request){
+	public String backLogin(@RequestParam Map<String,String> reqMap, HttpServletRequest request){
 
 		//调用service查找账户的方法
 		Backstage backstage = backStageService.queryBackStageAccount(reqMap);
 
+
+
 		//获取改角色类型的菜单目录
-		HashMap<String, ArrayList<RoleMenu>> menu = backMenuService.queryRoleMenu(backstage.getRid());
+		List<RoleMenu> roleMenus = backMenuService.queryRoleMenu(backstage.getRid());
+
+		System.out.println(roleMenus.toString());
 
 		//将该用户放入session域中
 		request.getSession().setAttribute("backstage",backstage);
-
-		//将该角色的菜单存入session域中
-		request.getSession().setAttribute("menu",menu);
-
-		System.out.println(menu.toString());
 
 		//响应前端
 		String msg = "";
@@ -57,7 +55,7 @@ public class BackStageController
 		}else {
 			msg = "2";
 		}
-		return menu;
+		return msg;
 	}
 
 	@RequestMapping("/logOut")
