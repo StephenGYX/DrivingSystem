838fd4bc58bbd798ac25dbcf38f2eeebb8c267cd
gkpk1560 <841126293@qq.com>
2020-02-16 20:50:09 +0800

    0216 gan

---
+++ src/main/java/com/drivingsys/bean/Drivingschool.java
@@ -0,0 +1,247 @@
+package com.drivingsys.bean;
+
+
+public class Drivingschool {
+
+  private long did;
+  private String daccount;
+  private String dpassword;
+  private String dname;
+  private String dcontacts;
+  private long dtelephone;
+  private long dphone;
+  private String demail;
+  private long dprovinceid;
+  private long dcityid;
+  private long dareaid;
+  private String daddress;
+  private String drecruit;
+  private long dprice;
+  private String dsynopsis;
+  private String daccountstate;
+  private String drecruitstate;
+  private long denrollnum;
+  private long dgraduationnum;
+  private long devaluatescore;
+  private long rid;
+  private String dregtime;
+  private String dremark1;
+  private String dremark2;
+
+
+  public long getDid() {
+    return did;
+  }
+
+  public void setDid(long did) {
+    this.did = did;
+  }
+
+
+  public String getDaccount() {
+    return daccount;
+  }
+
+  public void setDaccount(String daccount) {
+    this.daccount = daccount;
+  }
+
+
+  public String getDpassword() {
+    return dpassword;
+  }
+
+  public void setDpassword(String dpassword) {
+    this.dpassword = dpassword;
+  }
+
+
+  public String getDname() {
+    return dname;
+  }
+
+  public void setDname(String dname) {
+    this.dname = dname;
+  }
+
+
+  public String getDcontacts() {
+    return dcontacts;
+  }
+
+  public void setDcontacts(String dcontacts) {
+    this.dcontacts = dcontacts;
+  }
+
+
+  public long getDtelephone() {
+    return dtelephone;
+  }
+
+  public void setDtelephone(long dtelephone) {
+    this.dtelephone = dtelephone;
+  }
+
+
+  public long getDphone() {
+    return dphone;
+  }
+
+  public void setDphone(long dphone) {
+    this.dphone = dphone;
+  }
+
+
+  public String getDemail() {
+    return demail;
+  }
+
+  public void setDemail(String demail) {
+    this.demail = demail;
+  }
+
+
+  public long getDprovinceid() {
+    return dprovinceid;
+  }
+
+  public void setDprovinceid(long dprovinceid) {
+    this.dprovinceid = dprovinceid;
+  }
+
+
+  public long getDcityid() {
+    return dcityid;
+  }
+
+  public void setDcityid(long dcityid) {
+    this.dcityid = dcityid;
+  }
+
+
+  public long getDareaid() {
+    return dareaid;
+  }
+
+  public void setDareaid(long dareaid) {
+    this.dareaid = dareaid;
+  }
+
+
+  public String getDaddress() {
+    return daddress;
+  }
+
+  public void setDaddress(String daddress) {
+    this.daddress = daddress;
+  }
+
+
+  public String getDrecruit() {
+    return drecruit;
+  }
+
+  public void setDrecruit(String drecruit) {
+    this.drecruit = drecruit;
+  }
+
+
+  public long getDprice() {
+    return dprice;
+  }
+
+  public void setDprice(long dprice) {
+    this.dprice = dprice;
+  }
+
+
+  public String getDsynopsis() {
+    return dsynopsis;
+  }
+
+  public void setDsynopsis(String dsynopsis) {
+    this.dsynopsis = dsynopsis;
+  }
+
+
+  public String getDaccountstate() {
+    return daccountstate;
+  }
+
+  public void setDaccountstate(String daccountstate) {
+    this.daccountstate = daccountstate;
+  }
+
+
+  public String getDrecruitstate() {
+    return drecruitstate;
+  }
+
+  public void setDrecruitstate(String drecruitstate) {
+    this.drecruitstate = drecruitstate;
+  }
+
+
+  public long getDenrollnum() {
+    return denrollnum;
+  }
+
+  public void setDenrollnum(long denrollnum) {
+    this.denrollnum = denrollnum;
+  }
+
+
+  public long getDgraduationnum() {
+    return dgraduationnum;
+  }
+
+  public void setDgraduationnum(long dgraduationnum) {
+    this.dgraduationnum = dgraduationnum;
+  }
+
+
+  public long getDevaluatescore() {
+    return devaluatescore;
+  }
+
+  public void setDevaluatescore(long devaluatescore) {
+    this.devaluatescore = devaluatescore;
+  }
+
+
+  public long getRid() {
+    return rid;
+  }
+
+  public void setRid(long rid) {
+    this.rid = rid;
+  }
+
+
+  public String getDregtime() {
+    return dregtime;
+  }
+
+  public void setDregtime(String dregtime) {
+    this.dregtime = dregtime;
+  }
+
+
+  public String getDremark1() {
+    return dremark1;
+  }
+
+  public void setDremark1(String dremark1) {
+    this.dremark1 = dremark1;
+  }
+
+
+  public String getDremark2() {
+    return dremark2;
+  }
+
+  public void setDremark2(String dremark2) {
+    this.dremark2 = dremark2;
+  }
+
+}
