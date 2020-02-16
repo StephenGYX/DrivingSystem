29eb72bca0484a8a4be1ebc632ef87e7bdb22bc3
a576329350 <576329350@qq.com>
2020-02-16 14:34:50 +0800

    测试TestMapper

--- src/main/java/com/drivingsys/dao/TestMapper.java
+++ src/main/java/com/drivingsys/dao/TestMapper.java
@@ -1,4 +1,17 @@
 package com.drivingsys.dao;
 
+import com.drivingsys.bean.Practise;
+import com.drivingsys.bean.RoleMenu;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
 public interface TestMapper
-{}
+{
+	@Select("select * from practise where paccount=#{username} and ppassword=#{password}")
+	public Practise queryPractiseAccount(Map<String, String> map);
+
+}
