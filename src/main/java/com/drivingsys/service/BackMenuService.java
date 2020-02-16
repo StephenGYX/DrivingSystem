0a69aa7c4829403e8b2bf7cc9c66ee89d3167cbf
gkpk1560 <841126293@qq.com>
2020-02-16 14:48:18 +0800

    啥都没更新 就是做测试

--- src/main/java/com/drivingsys/service/BackMenuService.java
+++ src/main/java/com/drivingsys/service/BackMenuService.java
@@ -2,8 +2,6 @@ package com.drivingsys.service;
 
 import com.drivingsys.bean.RoleMenu;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public interface BackMenuService
@@ -13,7 +11,5 @@ public interface BackMenuService
 	 * 查找角色对应的id
 	 * @param rid
 	 */
-	public HashMap<String, ArrayList<RoleMenu>> queryRoleMenu(int rid);
-
-
+	public List<RoleMenu> queryRoleMenu(int rid);
 }
