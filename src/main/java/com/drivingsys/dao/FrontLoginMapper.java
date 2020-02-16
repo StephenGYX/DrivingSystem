cc0d31f6e47b2113b222418918c04a6fc966ef2c
gkpk1560 <841126293@qq.com>
2020-02-16 20:49:04 +0800

    0216 gan

--- src/main/java/com/drivingsys/dao/FrontLoginMapper.java
+++ src/main/java/com/drivingsys/dao/FrontLoginMapper.java
@@ -1,6 +1,8 @@
 package com.drivingsys.dao;
 
 import com.drivingsys.bean.Backstage;
+import com.drivingsys.bean.Consumer;
+import com.drivingsys.bean.Drivingschool;
 import com.drivingsys.bean.Practise;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -15,4 +17,10 @@ public interface FrontLoginMapper
 
 	@Select("select * from practise where paccount=#{username} and ppassword=#{password}")
 	public Practise queryPractiseAccount(Map<String, String> map);
+
+	@Select("select * from drivingschool where daccount=#{username} and dpassword=#{password}")
+	public Drivingschool queryDrivingschool(Map<String, String> map);
+
+	@Select("select * from consumer where caccount=#{username} and cpassword=#{password}")
+	public Consumer queryConsumer(Map<String, String> map);
 }
