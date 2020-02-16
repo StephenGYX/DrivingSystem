1c3c7158519477233b0ed3192546f54423c6fc1e
gkpk1560 <841126293@qq.com>
2020-02-16 14:30:38 +0800

    Merge branch 'master' of https://github.com/StephenGYX/DrivingSystem
    # Conflicts:
    #	src/main/java/com/drivingsys/controller/TestController.java

--- src/main/java/com/drivingsys/dao/TestMapper.java
+++ src/main/java/com/drivingsys/dao/TestMapper.java
@@ -1,4 +1,14 @@
 package com.drivingsys.dao;
 
+import com.drivingsys.bean.Practise;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Map;
+
 public interface TestMapper
-{}
+{
+
+	@Select("select * from practise where paccount=#{username} and ppassword=#{password}")
+	public Practise queryPractiseAccount(Map<String, String> map);
+
+}
