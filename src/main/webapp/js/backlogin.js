7870057478fad1acbf85f4e821cc958b900466a2
StephenGYX <471733780@qq.com>
2020-02-24 17:26:19 +0800

    2020/02/24

--- src/main/webapp/js/backlogin.js
+++ src/main/webapp/js/backlogin.js
@@ -7,6 +7,12 @@ window.onload = function () {
 	// alert(path+'/back/backLogin');
 };
 
+//改变验证码事件
+function changeImg() {
+	var img = document.getElementById('codeimg');
+	img.src = path + "/fact/getyzm?x=" + Math.floor(Math.random() * 100)
+};
+
 layui.use(['form'], function () {
 	var form = layui.form,
 		layer = layui.layer;
@@ -15,10 +21,10 @@ layui.use(['form'], function () {
 	if (top.location != self.location) top.location = self.location;
 
 	// 粒子线条背景
-	$(document).ready(function(){
+	$(document).ready(function () {
 		$('.layui-container').particleground({
-			dotColor:'#5cbdaa',
-			lineColor:'#5cbdaa'
+			dotColor: '#5cbdaa',
+			lineColor: '#5cbdaa'
 		});
 	});
 
@@ -27,37 +33,40 @@ layui.use(['form'], function () {
 		var formData = data.field;
 		var account = formData.account;
 		var password = formData.password;
-		var captcha = formData.captcha;
+		var code = formData.code;
+
+		console.log(formData);
 
 		if (formData.account === '') {
 			layer.msg('用户名不能为空');
 			return false;
-		}else if (formData.password === '') {
+		} else if (formData.password === '') {
 			layer.msg('密码不能为空');
 			return false;
 		} else if (formData.captcha === '') {
 			layer.msg('验证码不能为空');
 			return false;
-		}else {
-
+		} else {
 			$.ajax({
-				type:"post"
-				,url:path+'/back/backLogin'
+				type: "post"
+				, url: path + '/back/backLogin'
 				//预期服务器返回的数据类型;
-				,datatype:"text"
+				, datatype: "text"
 				//从该js会发出到服务器的数据
-				,data:{"account":account,"password":password,"captcha":captcha}
+				, data: {"account": account, "password": password, "code": code}
 				//从servlet接收的数据
-				,success:function (msg) {
-					if(msg==="1"){
+				, success: function (msg) {
+					if (msg === "1") {
 						layer.msg('登录成功', function () {
-							window.location = path+'/jsp/backmenu.jsp';
+							window.location = path + '/jsp/backmenu.jsp';
 						});
-					}else {
+					} else if (msg === "2") {
 						layer.msg("账号或密码有误，请重新输入");
+					} else if (msg === "3") {
+						layer.msg("验证码错误，请重新输入");
 					}
 				}
-				,error:function () {
+				, error: function () {
 					layer.alert("服务器正忙.....");
 				}
 			});
