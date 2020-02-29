000d8ff6501f6346a478d7538e672df78bdbb593
wonderponyo <470084183@qq.com>
2020-02-29 13:49:06 +0800

    ni

---
+++ src/main/java/com/drivingsys/bean/Monirecord.java
@@ -0,0 +1,57 @@
+package com.drivingsys.bean;
+
+
+public class Monirecord {
+
+  private String id;
+  private String cid;
+  private String testtime;
+  private String score;
+  private String kemu;
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public String getCid() {
+    return cid;
+  }
+
+  public void setCid(String cid) {
+    this.cid = cid;
+  }
+
+
+  public String getTesttime() {
+    return testtime;
+  }
+
+  public void setTesttime(String testtime) {
+    this.testtime = testtime;
+  }
+
+
+  public String getScore() {
+    return score;
+  }
+
+  public void setScore(String score) {
+    this.score = score;
+  }
+
+
+  public String getKemu() {
+    return kemu;
+  }
+
+  public void setKemu(String kemu) {
+    this.kemu = kemu;
+  }
+
+}
